afedfd132f274b24d963b62e580ee80340d2b22d
Sub2n <95su1208@gmail.com>
2020-01-16 13:15:44 +0900

    feat: html작업

--- src/components/MainWorks.js
+++ src/components/MainWorks.js
@@ -1,3 +1,16 @@
 import React from 'react';
 
-const MainWorks = () => {};
+const MainWorks = () => {
+  return (
+    <section className="works">
+      <h4>PROJECTS</h4>
+      <div className="row">
+        <div className="columns">
+          <ul className="grid"></ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MainWorks;
